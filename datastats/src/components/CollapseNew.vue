51aab2743c276e31a6709f434ab0ae7a3fc09649
uu <67761448@hotmail.com>
2024-05-06 17:09:09 +0800

    1

--- datastats/src/components/CollapseNew.vue
+++ datastats/src/components/CollapseNew.vue
@@ -1,123 +1,115 @@
-<script setup>
-import { reactive, ref, provide, inject } from "vue";
-/* const obj = inject("provideObj") */
-/* const obj = reactive({
-    group1: "group1",
-    list: [
-        { name: "1", Show: true, },
-        { name: "2", Show: true, }
-    ],
-    group3: "group3",
-    Show: true,
-
-}) */
-
-/* const obj = defineProps({
-    group: {
-        type: Array, // 指定类型为数组
-        required: true // 设置为必需属性
-    }
-}); */
-const menu = reactive([
-    {
-        group1: "g1",
-        group2: "g1-g1",
-        name: "g1g1n1",
-        show: true
-    },
-    {
-        group1: "g1",
-        group2: "g1-g1",
-        name: "g1g2n2",
-        show: true
-    },
-    {
-        group1: "g2",
-        group2: "g1",
-        name: "g2g1n1",
-        show: true
-    },
-    {
-        group1: "g2",
-        group2: "g2",
-        name: "g2g2n2",
-        show: true
-    },
-])
-
-//console.log(obj);
-//点击div，关闭下面的子菜单
-const collapseClick = (value) => {
-
-    for (let index = 0; index < group2Data.length; index++) {
-        const item = group2Data[index];
-        console.log(value);
-        if (item.father == value) {
-            item.element.show = !item.element.show
-        }
-    }
-
-}
-
-//存储group1
-const group1Set = new Set()
-const group2Set = new Set()
-//二级菜单
-const group2Data = [];
-//三级菜单
-const Group3eData = [];
-//获取第一层级的根菜单
-const getGroup1Set = () => {
-    for (let i = 0; i < menu.length; i++) {
-        const element = menu[i];
-        if (!group1Set.has(element.group1))
-            group1Set.add(element.group1)
-    }
-}
-//获取第二级菜单
-const getGroup2Set = () => {
-    for (let i = 0; i < menu.length; i++) {
-        const element = menu[i];
-
-        const group2Item = { father: element.group1, name: element.group2 };
-        if (!group2Set.has(group2Item)) {
-            group2Set.add(group2Item); 
-        }
-    }
-}
-const recurve = ()=>{
-    for (const element of group2Set) {
-        console.log(element.father);
-    }
-}
-
-getGroup1Set()
-getGroup2Set()
-recurve()
-/* getGrou1() */
-console.log(group1Set);
-//console.log(group2Set);
-</script>
-
 <template>
-    <!--        <div v-for="group1 in group1Set" class="collapse" @click.stop="collapseClick(group1)">
-        group1:{{ group1 }}
-        <div v-for="group2 in group2Data">
-            <div v-if="group1 === group2.father">
-                <div @click.stop v-show="group2.element.show">g2:{{ group2.father }}</div>
-
+    <div>
+        <div v-for="group1 in groupedMenu" :key="group1.group1" class="collapse" @click.stop="toggleCollapse(group1)">
+            <span :class="['arrow', { 'rotate': !group1.expanded }]">&gt;</span> group1: {{ group1.group1 }}
+            <div v-show="group1.expanded">
+                <div v-for="group2 in group1.children" :key="group2.group2" class="collapse"
+                    @click.stop="toggleCollapse(group2)">
+                    <span :class="['arrow', { 'rotate': !group2.expanded }]">&gt;</span> group2: {{ group2.group2 }}
+                    <div v-show="group2.expanded">
+                        <div v-for="group3 in group2.children" :key="group3.group3" class="collapse"
+                            @click.stop="toggleCollapse(group3)">
+                            <span :class="['arrow', { 'rotate': !group3.expanded }]">&gt;</span> group3: {{
+                            group3.group3 }}
+                            <div v-show="group3.expanded">
+                                <div v-for="item in group3.children" :key="item.name" class="collapse"
+                                    @click.stop="toggleFontColor(item)">
+                                    <span :class="{ 'green': item.selected }">{{ item.name }}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>  -->
-   <div v-for="group1 of group1Set" class="collapse" @click.stop="collapseClick(group1)">
-        group1:{{ group1 }}
-        <div v-for="group2 of group2Set">
-            <div v-if="group1 === group2.value.father">
-                <div @click.stop v-show="group2.element.show">g2:{{ group2.name }}</div>
-
-            </div>
-        </div>
-    </div> 
+    </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import { reactive, ref } from 'vue';
+
+const menu = reactive([
+    { group1: "g1", group2: "g1-g1", group3: "g321", name: "g1g1n1", selected: false },
+    { group1: "g1", group2: "g1-g1", group3: "g321", name: "g1g2n2", selected: false },
+    { group1: "g2", group2: "g1", group3: "g312", name: "g2g1n1", selected: false },
+    { group1: "g2", group2: "g2", group3: "g322", name: "g2g2n2", selected: false }
+]);
+
+const groupedMenu = ref([]);
+
+const initializeGroupedMenu = () => {
+    const groupedByGroup1 = new Map();
+    for (const item of menu) {
+        if (!groupedByGroup1.has(item.group1)) {
+            groupedByGroup1.set(item.group1, []);
+        }
+        groupedByGroup1.get(item.group1).push(item);
+    }
+
+    for (const [group1, items] of groupedByGroup1) {
+        const group1Item = { group1, expanded: false, children: [] };
+        const groupedByGroup2 = new Map();
+        for (const item of items) {
+            if (!groupedByGroup2.has(item.group2)) {
+                groupedByGroup2.set(item.group2, []);
+            }
+            groupedByGroup2.get(item.group2).push(item);
+        }
+        for (const [group2, group2Items] of groupedByGroup2) {
+            const group2Item = { group2, expanded: false, children: [] };
+            const groupedByGroup3 = new Map();
+            for (const item of group2Items) {
+                if (!groupedByGroup3.has(item.group3)) {
+                    groupedByGroup3.set(item.group3, []);
+                }
+                groupedByGroup3.get(item.group3).push(item);
+            }
+            for (const [group3, group3Items] of groupedByGroup3) {
+                const group3Item = { group3, expanded: false, children: group3Items };
+                group2Item.children.push(group3Item);
+            }
+            group1Item.children.push(group2Item);
+        }
+        groupedMenu.value.push(group1Item);
+    }
+};
+
+const toggleCollapse = (item) => {
+    item.expanded = !item.expanded;
+};
+
+const toggleFontColor = (clickedItem) => {
+    for (const group1 of groupedMenu.value) {
+        for (const group2 of group1.children) {
+            for (const group3 of group2.children) {
+                for (const item of group3.children) {
+                    item.selected = (item === clickedItem);
+                }
+            }
+        }
+    }
+};
+
+initializeGroupedMenu();
+</script>
+
+<style scoped>
+.collapse {
+    cursor: pointer;
+    margin-left: 10px;
+}
+
+.arrow {
+    display: inline-block;
+    margin-right: 5px;
+    transition: transform 0.3s;
+}
+
+.rotate {
+    transform: rotate(-90deg);
+}
+
+.green {
+    color: green;
+}
+</style>
