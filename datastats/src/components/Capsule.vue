1f40e26942e5f47d0847cbe4e16c3d204901fbc5
uu <67761448@hotmail.com>
2024-05-13 15:25:37 +0800

    capsule-OK

---
+++ datastats/src/components/Capsule.vue
@@ -0,0 +1,92 @@
+<template>
+    <canvas ref="canvasRef" width="300" height="13"></canvas>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+const props = defineProps({
+    data: {
+        type: Object,
+        required: true
+    }
+});
+
+const canvasRef = ref(null);
+
+onMounted(() => {
+    drawCapsule();
+});
+
+const drawCapsule = () => {
+    const canvas = canvasRef.value;
+    const ctx = canvas.getContext('2d');
+
+    const x = 0; // 矩形左上角 x 坐标
+    const y = 0; // 矩形左上角 y 坐标
+    const width = canvas.width - 50; // 矩形宽度
+    const height = 13; // 矩形高度
+    const borderRadius = height / 2; // 圆角半径
+
+    // 开始绘制路径
+    ctx.beginPath();
+
+    // 绘制左侧圆形
+    ctx.arc(x + borderRadius, y + borderRadius, borderRadius, Math.PI * 0.5, Math.PI * 1.5);
+
+    // 绘制矩形的右上角
+    ctx.lineTo(x + width - borderRadius, y);
+    ctx.arcTo(x + width, y, x + width, y + borderRadius, borderRadius);
+
+    // 绘制矩形的右下角
+    ctx.lineTo(x + width, y + height - borderRadius);
+    ctx.arcTo(x + width, y + height, x + width - borderRadius, y + height, borderRadius);
+
+    // 绘制右侧圆形
+    ctx.lineTo(x + borderRadius, y + height);
+    ctx.arc(x + borderRadius, y + height - borderRadius, borderRadius, Math.PI * 0, Math.PI * 0.5);
+
+    // 完成路径绘制
+    ctx.closePath();
+
+    // 填充矩形
+    ctx.fillStyle = '#242C44';
+    ctx.fill();
+
+
+
+
+    // 绘制路径2
+    ctx.beginPath();
+
+    // 绘制左侧圆形
+    ctx.arc(x + borderRadius, y + borderRadius, borderRadius, Math.PI * 0.5, Math.PI * 1.5);
+
+    // 绘制矩形的右上角
+    ctx.lineTo(x + props.data.width2 - borderRadius, y);
+    ctx.arcTo(x + props.data.width2, y, x + props.data.width2, y + borderRadius, borderRadius);
+
+    // 绘制矩形的右下角
+    ctx.lineTo(x + props.data.width2, y + height - borderRadius);
+    ctx.arcTo(x + props.data.width2, y + height, x + props.data.width2 - borderRadius, y + height, borderRadius);
+
+    // 绘制右侧圆形
+    ctx.lineTo(x + borderRadius, y + height);
+    ctx.arc(x + borderRadius, y + height - borderRadius, borderRadius, Math.PI * 0, Math.PI * 0.5);
+
+    // 完成路径绘制
+    ctx.closePath();
+
+    // 填充矩形
+    ctx.fillStyle = props.data.color;
+    ctx.fill();
+    // 添加中间数字
+    ctx.font = '0.8rem Microsoft YaHei';
+    ctx.fillStyle = '#fff';
+    ctx.fillText(props.data.middleNum, width / 2 - 8, height - 1);
+
+    // 添加右边数字
+    ctx.font = '0.8rem Microsoft YaHei';
+    ctx.fillStyle = '#fff';
+    ctx.fillText(props.data.rightNum, width + 18, height - 2);
+};
+</script>
--- datastats/src/components/Collapse.vue
+++ datastats/src/components/Collapse.vue
@@ -2,41 +2,59 @@
     <div class="moduleStyle">
         <div v-for="group1 in groupedMenu" :key="group1.group1" class="collapse" @click.stop="toggleCollapse(group1)">
             <div class="collapse-wrapper">
-                <div :class="['group-text', { 'selected': group1.expanded }]">
-                    {{ group1.group1 }}
-                    <span :class="['arrow', { 'rotate': !group1.expanded }]">&gt;</span>
+                <div class="collapse-group1">
+                    <div :class="['group-text', { 'selected': group1.expanded }]">
+                        {{ group1.group1 }}
+                        <span :class="['arrow', { 'rotate': !group1.expanded }]">&gt;</span>
+                    </div>
                 </div>
-                <div class="capsule"></div>
+                <div>
+                    <Capsule :data="dataCapsule"></Capsule>
+                </div>
             </div>
             <div v-show="group1.expanded">
                 <div v-for="group2 in group1.children" :key="group2.group2" class="collapse"
                     @click.stop="toggleCollapse(group2)">
-                    <div class="collapse-wrapper">
-                        <div :class="['group-text', { 'selected': group2.expanded }]">
-                            {{ group2.group2 }}
-                            <span :class="['arrow', { 'rotate': !group2.expanded }]">&gt;</span>
+                    <div class="collapse-wrapper ">
+                        <div class="collapse-group2">
+                            <div :class="['group-text', { 'selected': group2.expanded }]">
+                                {{ group2.group2 }}
+                                <span :class="['arrow', { 'rotate': !group2.expanded }]">&gt;</span>
+                            </div>
                         </div>
-                        <div class="capsule"></div>
+                        <div>
+                            <Capsule :data="dataCapsule"></Capsule>
+                        </div>
                     </div>
                     <div v-show="group2.expanded">
                         <div v-for="group3 in group2.children" :key="group3.group3" class="collapse"
                             @click.stop="toggleCollapse(group3)">
-                            <div class="collapse-wrapper">
-                                <div :class="['group-text', { 'selected': group3.expanded }]">
-                                    {{ group3.group3 }}
-                                    <span :class="['arrow', { 'rotate': !group3.expanded }]">&gt;</span>
+                            <div class="collapse-wrapper ">
+                                <div class="collapse-group3">
+                                    <div :class="['group-text', { 'selected': group3.expanded }]">
+                                        {{ group3.group3 }}
+                                        <span :class="['arrow', { 'rotate': !group3.expanded }]">&gt;</span>
+                                    </div>
                                 </div>
-                                <div class="capsule"></div>
+                                <div>
+                                    <Capsule :data="dataCapsule"></Capsule>
+                                </div>
                             </div>
                             <div v-show="group3.expanded">
                                 <div v-for="item in group3.children" :key="item.name" class="collapse"
                                     @click.stop="toggleFontColor(item)">
-                                    <div class="item-wrapper">
-                                        <div :class="{ 'selected': item.selected }" @click="logItemId(item.id)">
-                                            <span :class="{ 'mark1': item.selected, 'mark2': !item.selected }"
-                                                style="margin-right: 0.5rem;"></span>{{ item.name }}
+                                    <div class="collapse-wrapper ">
+                                        <div class="collapse-item">
+                                            <div :class="['group-text', { 'selected': item.selected }]"
+                                                @click="logItemId(item.id)">
+
+                                                <div class="marquee"><span>&bull;&nbsp;&nbsp;{{ item.name }}</span>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div class="capsule"></div>
+                                        <div>
+                                            <Capsule :data="dataCapsule"></Capsule>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -49,7 +67,8 @@
 </template>
 
 <script setup>
-import { ref, defineProps } from 'vue';
+import { ref } from 'vue';
+import Capsule from './Capsule.vue';
 
 const props = defineProps({
     menu: {
@@ -57,7 +76,12 @@ const props = defineProps({
         required: true
     }
 });
-
+const dataCapsule = {
+    middleNum: "45%",
+    rightNum: 9999,
+    width2: 130,
+    color: "#F2B564",
+};
 const groupedMenu = ref([]);
 
 const initializeGroupedMenu = () => {
@@ -102,6 +126,7 @@ const toggleCollapse = (item) => {
 };
 
 const toggleFontColor = (clickedItem) => {
+    scrollToBottom();
     for (const group1 of groupedMenu.value) {
         for (const group2 of group1.children) {
             for (const group3 of group2.children) {
@@ -114,20 +139,41 @@ const toggleFontColor = (clickedItem) => {
 };
 
 const logItemId = (itemId) => {
+    scrollToBottom();
     console.log("item id:", itemId);
 };
 
 initializeGroupedMenu();
+console.log(groupedMenu);
+
+
+
+const scrollToBottom = () => {
+    const container = document.querySelector('.moduleStyle');
+    container.scrollTop = container.scrollHeight;
+}
+
+
+
 </script>
 
 <style scoped>
-.collapse {
-    cursor: pointer;
-    margin-top: 1px;
+.moduleStyle {
+    color: #FCFCFC;
+    padding: 0rem 1rem 0rem 1.3rem;
+    overflow-y: auto;
+    overflow-x: hidden;
 }
 
-.group-text {
-    padding-right: 10px;
+.collapse {
+    cursor: pointer;
+    margin-top: 10px;
+}
+
+.group-text-wrapper {
+    flex: 2;
+    border: 1px solid #ccc;
+
 }
 
 .arrow {
@@ -144,34 +190,88 @@ initializeGroupedMenu();
 }
 
 .mark1 {
+    margin-bottom: 0.25rem;
     display: inline-block;
-    width: 8px;
-    height: 8px;
+    width: 5px;
+    height: 5px;
     background-color: #6FF8F4;
     border-radius: 4px;
 }
 
 .mark2 {
+    margin-bottom: 0.25rem;
     display: inline-block;
-    width: 8px;
-    height: 8px;
+    width: 5px;
+    height: 5px;
     background-color: #F3F4F5;
     border-radius: 4px;
 }
 
-.capsule {
-    width: 5rem;
-    height: 10px;
-    background-color: #488EF7;
-    border-radius: 4px;
-    margin-left: 20px;
+.collapse-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
 }
 
-.moduleStyle {
-    color: #F3F4F5;
-    height: 23.9rem;
-    overflow-y: auto;
-    overflow-x: hidden;
+
+.collapse-group1 {
+    margin-right: 40px;
+    width: 188px;
+}
+
+
+.collapse-group2 {
+    margin-left: 10px;
+    margin-right: 30px;
+    width: 188px;
+}
+
+.collapse-group3 {
+    margin-left: 20px;
+    margin-right: 20px;
+    width: 188px;
+}
+
+.collapse-item {
+    margin-left: 30px;
+    margin-right: 10px;
+    white-space: nowrap;
+    overflow: hidden;
+    width: 188px;
+}
+
+.my-capsule {
+    flex: 5;
+    width: 80%;
+    height: 13px;
+
+}
+
+.capsule1 {
+    flex: 5;
+    width: 80%;
+    height: 13px;
+    background-color: #242C44;
+    border-radius: 17px;
+
+}
+
+.marquee:hover {
+    display: flex;
+    width: 190px;
+    resize: horizontal;
+    container-type: inline-size;
+    align-items: baseline;
+
+    >span {
+        animation: marquee 5s linear infinite both alternate;
+    }
+}
+
+@keyframes marquee {
+    to {
+        transform: translateX(min(100cqw - 100%, 0px));
+    }
 }
 
 ::-webkit-scrollbar {
